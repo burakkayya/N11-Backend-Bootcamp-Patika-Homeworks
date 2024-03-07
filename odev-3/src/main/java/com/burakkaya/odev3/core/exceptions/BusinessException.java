b5ea8ac27bcde612775bfe92a923459e73ad54ea
Esref Burak Kaya <ebkaya.0637@gmail.com>
2024-03-07 03:16:36 +0300

    Exception handling is added.
    Exception handling is added.

---
+++ odev-3/src/main/java/com/burakkaya/odev3/core/exceptions/BusinessException.java
@@ -0,0 +1,7 @@
+package com.burakkaya.odev3.core.exceptions;
+
+public class BusinessException extends RuntimeException{
+    public BusinessException(String message) {
+        super(message);
+    }
+}
